ce83f9a464332fd0de6de94624e418b54ad11ab3
Paul Yates <paul@debbie>
2018-03-01 23:50:21 -0500

    Incomplete implementation of main and data structures

--- main.cpp
+++ main.cpp
@@ -11,7 +11,17 @@ int main(int argc, char **argv) {
     std::cout << "Hello, RWClustering World!" << std::endl;
     //todo: interpret command-line arguments for input file and cluster size limit
 
-    int* delay_matrix;
+    std::list<Node> master; //to be filled by reading the blif file
+    int* delay_matrix; //TODO: allocate me
+
+    std::list<Node>::const_iterator n; //the current node
+    std::vector<Node>::const_iterator p; //the node's predecessors
+    for(n = master.begin(); n != master.end();++n){
+        int currentMax=0;
+        for(p = p->prev.begin(); p != p->prev.end(); ++p){
+            if(
+        }
+    }
     std::vector<Node> PIs;
     std::vector<Node> POs;
     std::vector<Node> L = POs; //L starts equal to the PO set
